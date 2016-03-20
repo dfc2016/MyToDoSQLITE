977ebc425a90543a0366b360931364fa9350f8f3
diego corrada <diego.corrada@gmail.com>
2016-03-20 02:30:18 +0000

    TODO: Synchronize the doLogin(...) with the running state of the App
    Changes made by diego corrada (diego.corrada@gmail.com).

--- My ToDo/components/home/index.js
+++ My ToDo/components/home/index.js
@@ -19,6 +19,9 @@ app.home = kendo.observable({
             StartUser: "",
             StartPassword: "",
         },
+        registrar: {
+            isVisible: false,
+        },
         getUsrPwd: function() {
             $("#sqlengine").html(app.dbEngine);
             app.db.transaction(
@@ -39,6 +42,20 @@ app.home = kendo.observable({
         LogIn: function() {
             //console.log("Log in for: "+$("#usr").val()+" // "+$("#pwd").val());
             console.log("MVVM >>> Log in for: "+startModel.fields.StartUser+" // "+startModel.fields.StartPassword);
+            var userfound;
+            userfound = doLogin(
+                startModel.fields.StartUser,
+                startModel.fields.StartPassword
+            );
+            console.log("MVVM >>> userfound: "+userfound);
+            userfound = false;
+            if (userfound == true) {
+                // Go to inital view
+            }
+            if (userfound == false) {
+                // Show register option
+                parent.set("startModel.registrar.isVisible", true);
+            }
         },
     });
     parent.set('startModel', startModel);
@@ -46,16 +63,41 @@ app.home = kendo.observable({
 })(app.home);
 // END_CUSTOM_CODE_home
 
+/*
+* DFC 20160319 +++ VERY VERY IMPORTANT +++
+* THE app.db.transaction (...) FUNCTION API IS BASED ON 
+* A CALL BACK ASYNCHROUNOUS REFACTOR THE CODE FOR SYNCHRONIZE 
+* THE doLogin(...) FX 
+* TIP: MOVE THE CODE INSIDE THE kendo.observable(...)
+*
+*/
 function doLogin(user,password) {
-	app.db.transaction   {
+    console.log("DFC >>> doLogin(...) >>> START ");
+    var userOk = false;
+	app.db.transaction   (
         function(tx) {
             tx.executeSql(
                 "SELECT COUNT(*) AS USERFOUND FROM users WHERE user=? AND  password=?",
                 [user,password],
                 function(tx, rs) {
                     //DFC 20160319 TODO: IMPLEMENT HERE THE CHECK POINT IF USR EXISTS...
+                    console.log("DFC >>> doLogin(...) >>> USERFOUND: "+rs.rows.item(0).USERFOUND);
+                    var userfound;
+                    userfound = Number(rs.rows.item(0).USERFOUND);
+                    if(userfound == 1){
+                        userOk = true;
+                        console.log("DFC >>> doLogin(...) >>> user OK ");
+                    }
+                    if(userfound == 0){
+                        userOk = false;
+                        console.log("DFC >>> doLogin(...) >>> user KO!!! ");
+                    }
+                    waitForSyncLogin = false;
+                    //return rs.rows.item(0).USERFOUND;
                 }
             )
         }
-    }
+    );
+    console.log("DFC >>> doLogin(...) >>> END ");
+    return userOk;
 }
